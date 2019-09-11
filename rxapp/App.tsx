ebb651c39f89f535ff6451cd3f076da93bef6e81
Pavlos Vinieratos <pvinis@gmail.com>
2019-09-11 19:09:34 +0200

    base

--- rxapp/App.tsx
+++ rxapp/App.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {  of, Subject, merge } from 'rxjs';
+import { connect, combineProps, useRxController } from 'rx-react-container';
+import { map, scan, switchMap, startWith } from 'rxjs/operators';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  function appController(container) {
+    const onMinus = new Subject();
+    const onPlus = new Subject();
+
+    const click = merge(
+      onMinus.pipe(map(() => -1)),
+      onPlus.pipe(map(() => +1))
+    );
+    const step = of(1)
+
+    const totalCount = step.pipe(
+      switchMap(step => click.pipe(map(v => v * step))),
+      startWith(0),
+      scan((acc, x) => acc + x, 0)
+    );
+
+    return combineProps(
+      { totalCount , },
+      { onMinus , onPlus  }
+    );
+  }
+
+ export default function App(props) {
+    const state = useRxController(appController, props);
+    if(!state) return null;
+    const { onMinus, onPlus, totalCount, } = state;
+    return (
+        <View style={{marginTop: 100}}>
+        <Button onPress={onMinus} title='-' />
+        <Text>{totalCount}</Text>
+        <Button onPress={onPlus} title='+' />
+      </View>
+    );
+  }
