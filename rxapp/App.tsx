5397d7796e391c574522399d093bc93cd7ced2ed
Pavlos Vinieratos <pvinis@gmail.com>
2019-09-20 19:03:24 +0200

    nav

--- rxapp/App.tsx
+++ rxapp/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { createAppContainer, FlatList } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
 import { Count } from './src/Count'
 import { SteppedCount } from './src/SteppedCount'
 import { SequenceDetector } from './src/SequencerDetector'
+import { Button } from 'react-native'
 
 
-export default function App() {
-	return <Count />
-	return <SteppedCount />
-	return <SequenceDetector />
-}
+const List = (props) => (
+	<>
+		<Button title='Count' onPress={() => props.navigation.navigate('Count')} />
+		<Button title='Stepped Count' onPress={() => props.navigation.navigate('SteppedCount')} />
+		<Button title='Sequence Detector' onPress={() => props.navigation.navigate('SequenceDetector')} />
+	</>
+)
+
+const AppNavigator = createStackNavigator({
+	List: { screen: List },
+	Count: { screen: Count },
+	SteppedCount: { screen: SteppedCount },
+	SequenceDetector: { screen: SequenceDetector },
+})
+
+export default createAppContainer(AppNavigator)
