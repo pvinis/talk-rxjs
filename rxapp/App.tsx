85ab563a066fe01b491623ba0810b8534b0d4da6
Pavlos Vinieratos <pvinis@gmail.com>
2019-09-20 18:26:45 +0200

    seq

--- rxapp/App.tsx
+++ rxapp/App.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 import { Count } from './src/Count'
 import { SteppedCount } from './src/SteppedCount'
+import { SequenceDetector } from './src/SequencerDetector'
 
 
 export default function App() {
-	return <SteppedCount />
+	return <SequenceDetector />
 }
---
+++ rxapp/src/SequencerDetector.tsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import { View, Text, Button, SafeAreaView } from 'react-native'
+import R from 'ramda'
+import { Subject, merge, Observable } from 'rxjs'
+import { combineProps, useRxController } from 'rx-react-container'
+import { map, scan, startWith, withLatestFrom } from 'rxjs/operators'
+
+import { RED } from './RED'
+
+
+enum Direction {
+    Left = 'LEFT',
+    Right = 'RIGHT',
+    Up = 'UP',
+    Down = 'DOWN',
+}
+
+type Sequence = Direction[]
+
+const Sequences = {
+	Left: [Direction.Left],
+	Konami: [
+		Direction.Up,
+		Direction.Up,
+		Direction.Down,
+		Direction.Down,
+		Direction.Left,
+		Direction.Right,
+		Direction.Left,
+		Direction.Right,
+	],
+}
+
+
+const controller = container => {
+	const onLeft = new Subject()
+	const onRight = new Subject()
+	const onUp = new Subject()
+	const onDown = new Subject()
+	// const onDirection = new Subject()
+
+	const direction: Observable<Direction> = merge(
+		onLeft.pipe(map(() => Direction.Left)),
+		onRight.pipe(map(() => Direction.Right)),
+		onUp.pipe(map(() => Direction.Up)),
+		onDown.pipe(map(() => Direction.Down)),
+	)
+
+	const sequenceSoFar: Observable<Sequence> = direction.pipe(
+		scan((acc, direction) => R.concat(acc, [direction]), [] as Sequence),
+		startWith([]),
+	)
+
+	const recognizedSequence = sequenceSoFar.pipe(
+		map(seq => {
+			if (R.equals(seq, Sequences.Konami)) return 'Konami'
+			if (R.equals(seq, Sequences.Left)) return 'Left!'
+			return ''
+		})
+	)
+
+	return combineProps(
+		{ sequenceSoFar, recognizedSequence },
+		{ onLeft, onRight, onUp, onDown },
+	)
+}
+
+export const SequenceDetector = props => {
+	const state = useRxController(controller, props)
+	if (!state) return <RED />
+
+	const {
+		onLeft, onRight, onUp, onDown,
+		sequenceSoFar, recognizedSequence,
+		// some callback to do somethins
+	} = state
+
+	return (
+		<SafeAreaView style={{ alignItems: 'center', marginTop: 200 }}>
+			<View style={{ height: 200, width: 300 }}>
+				<View style={{ position: 'absolute', top: 0, left: 150 }}><Button onPress={onUp} title='up' /></View>
+				<View style={{ position: 'absolute', bottom: 0, left: 150 }}><Button onPress={onDown} title='down' /></View>
+				<View style={{ position: 'absolute', left: 0, top: 100 }}><Button onPress={onLeft} title='left' /></View>
+				<View style={{ position: 'absolute', right: 0 , top: 100 }}><Button onPress={onRight} title='right' /></View>
+			</View>
+			<View style={{ alignItems: 'center' }}>
+				<Text>sequenceSoFar</Text>
+				<Text>{sequenceSoFar.join(', ')}</Text>
+				<Text>recognizedSequence</Text>
+				<Text>{recognizedSequence}</Text>
+			</View>
+		</SafeAreaView>
+	)
+}
